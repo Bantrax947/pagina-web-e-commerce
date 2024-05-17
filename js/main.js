e52f70ee0b4a4c65f04ded099cf8fffac09cd1e4
Bantrax947 <francofabian947@gmail.com>
2024-05-17 13:50:54 -0400

    Se agregaron los arreglos para los productos

---
+++ js/main.js
@@ -0,0 +1,222 @@
+/*Productos*/
+const productos =[
+    /*Array de Abrigos */
+    {
+
+        id: "abrigo-01",
+        tiutlo: "Abrigo 01",
+        imagen: "./img/abrigos/01.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "abrigo-02",
+        tiutlo: "Abrigo 02",
+        imagen: "./img/abrigos/02.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "abrigo-03",
+        tiutlo: "Abrigo 03",
+        imagen: "./img/abrigos/03.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "abrigo-04",
+        tiutlo: "Abrigo 04",
+        imagen: "./img/abrigos/04.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "abrigo-05",
+        tiutlo: "Abrigo 05",
+        imagen: "./img/abrigos/05.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+
+    },
+    
+    /*Array de Camisetas */
+
+    {
+
+        id: "camiseta-01",
+        tiutlo: "Camiseta 01",
+        imagen: "./img/camisetas/01.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-02",
+        tiutlo: "Camiseta 02",
+        imagen: "./img/camisetas/02.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-03",
+        tiutlo: "Camiseta 03",
+        imagen: "./img/camisetas/03.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-04",
+        tiutlo: "Camiseta 04",
+        imagen: "./img/camisetas/04.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-05",
+        tiutlo: "Camiseta 05",
+        imagen: "./img/camisetas/05.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-06",
+        tiutlo: "Camiseta 06",
+        imagen: "./img/camisetas/06.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-07",
+        tiutlo: "Camiseta 07",
+        imagen: "./img/camisetas/07.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+    {
+
+        id: "camiseta-08",
+        tiutlo: "Camiseta 08",
+        imagen: "./img/camisetas/08.jpg",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 1000
+
+    },
+
+    /*Array de Pantalones */
+
+    {
+
+        id: "pantalon-01",
+        tiutlo: "Pantalon 01",
+        imagen: "./img/pantalones/01.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+
+        id: "pantalon-02",
+        tiutlo: "Pantalon 02",
+        imagen: "./img/pantalones/02.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+
+        id: "pantalon-03",
+        tiutlo: "Pantalon 03",
+        imagen: "./img/pantalones/03.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+
+        id: "pantalon-04",
+        tiutlo: "Pantalon 04",
+        imagen: "./img/pantalones/04.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+    {
+
+        id: "pantalon-05",
+        tiutlo: "Pantalon 05",
+        imagen: "./img/pantalones/05.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 1000
+    },
+
+];
